ac85ee4efe226ad6af8d990fb3b77675bb501448
Alex McGraw <jamcgraw21@gmail.com>
2025-02-16 13:25:30 -0600

    initial commits to my portfolio website. this is built off the template from this link https://vercel.com/templates/next.js/nextfolio-a-simple-next-js-portfolio

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Alex McGraw",
+  name: "Alex McGraw",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
@@ -11,6 +11,6 @@ export const socialLinks = {
   twitter: "https://x.com/1tssirius",
   github: "https://github.com/1msirius/Nextfolio",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/josephalexmcgraw/",
+  email: "jamcgraw21@gmail.com",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 import Image from "next/image";
+import profileImage from "@/public/Dubrovnik-2022.png";
 import { socialLinks } from "./config";
 
 export default function Page() {
+  const workIveDone = [
+    "Architect GenAI data analyst agent for finance and merchandise managers within CVS Retail to analyze data with natural language",
+    "Built said agent in Python using Burr, DeepEval, Streamlit, OpenAI, Azure and Snowflake",
+    "Developed a Jira ticket to DAX code GenAI conversion system",
+    `Implement a multi-camera computer vision solution using open source models and AWK Rekognition to track individuals' emotion, 
+    eye gaze, and subsequent walking route when looking at an information board`,
+    "Wrote, tested, and productionalized customer churn models in Azure"
+  ]
+
+  const sideProjects = [
+    "Built a basic cover letter generator using Python, FastAPI, and OpenAI on the backend and TypeScript and React on the frontend",
+    "Contributed to LlamaIndex to make connections to Azure Blob Storage via credentials and enhanced PDF Reader functionality to give users more control over doc structure"
+  ]
+
+  const interests = [
+    "Building AI native features and products",
+    "Backend engineering with Python",
+    "Continuing to learn and build frontends with React/NextJS and TypeScript",
+    "Helping solve problems that make a positive difference for others and/or bring enjoyment to others"
+  ]
+
   return (
     <section>
       <a href={socialLinks.twitter} target="_blank">
         <Image
-          src="/profile.png"
+          src={profileImage}
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
@@ -16,49 +38,47 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio template!
+        About Me
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
+        <h2>
+          Hey I'm <div className="text-blue-600 inline">Alex.</div> Welcome to my personal site where you can find my professional experiences and
+          interests, along with personal experiences and other random tid bits.
+        </h2>
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS.
+        I am currently a Senior Decision Scientist at CVS Health and was previously a Data & Technology Consultant at Slalom Consulting. 
+        Job titles can sometimes be misleading about what I've actually done and, more importantly, what truly interests me.
         </p>
         <p>
-          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
-          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
+         <b>Here is a glimpse at what I've done professionally.</b>
         </p>
+        <ul className="gap-4 p-4">
+            {workIveDone.map((item, index) => (
+            <li key={index} className="w-full bg-gray-200 p-2 rounded-md text-center">
+              {item}
+            </li>
+          ))}
+          </ul>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
+          <b>Here are a few things that I've done on the side.</b>
         </p>
+        <ul className="gap-4 p-4">
+            {sideProjects.map((item, index) => (
+              <li key={index} className="w-full bg-gray-200 p-2 rounded-md text-center">
+                {item}
+              </li>
+            ))}
+          </ul>
         <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
+          <b>Here is what I am interested in doing for work and where I truly want to grow</b>
         </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
-        </p>
+        <ul className="gap-4 p-4">
+            {interests.map((item, index) => (
+              <li key={index} className="w-full bg-gray-200 p-2 rounded-md text-center">
+                {item}
+              </li>
+            ))}
+          </ul>
       </div>
     </section>
   );
